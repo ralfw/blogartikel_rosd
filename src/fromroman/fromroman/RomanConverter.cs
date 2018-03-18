a8feca72f1b7889c7b4dea79298b6cb2ce9e2e0d
Ralf Westphal <info@ralfw.de>
2018-03-18 15:29:07 +0100

    Parse() implementiert
    Es fehlt allerdings ein Test.

--- src/fromroman/fromroman/RomanConverter.cs
+++ src/fromroman/fromroman/RomanConverter.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Linq;
 
 namespace fromroman
 {
     public class RomanConverter
     {
         public static int Convert(string roman) { throw new NotImplementedException();}
+
+        internal static int[] Parse(string roman)
+        {
+            var digits = roman.ToCharArray();
+            return digits.Select(MapToValue).ToArray();
+
+            int MapToValue(char digit)
+            {
+                switch (char.ToUpper(digit))
+                {
+                    case 'I': return 1;
+                    case 'V': return 5;
+                    case 'X': return 10;
+                    case 'L': return 50;
+                    case 'C': return 100;
+                    case 'D': return 500;
+                    case 'M': return 1000;
+                    default: throw new InvalidOperationException($"Unknown digit: {digit}!");
+                }
+            }
+        }
     }
 }
