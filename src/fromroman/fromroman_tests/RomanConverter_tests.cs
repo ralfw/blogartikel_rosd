185bb02b9bbd0ef34caa20a594233e2b29281423
Ralf Westphal <info@ralfw.de>
2018-03-18 15:53:14 +0100

    Test für Zahl mit nur 1 Ziffer
    close #7

--- src/fromroman/fromroman_tests/RomanConverter_tests.cs
+++ src/fromroman/fromroman_tests/RomanConverter_tests.cs
@@ -9,7 +9,8 @@ namespace fromroman_tests
     {
         [TestCase("MCMLXXXIV", 1984)]
         [TestCase("MCDXCII", 1492)]
-        [TestCase("XVI", 16)]
+        [TestCase("XVI", 16)] // nur Addition von Ziffernwerten
+        [TestCase("M", 1000)] // nur 1 röm. Ziffer
         public void Akzeptanztests(string roman, int translation) {
             Assert.AreEqual(translation, RomanConverter.Convert(roman));
         }
